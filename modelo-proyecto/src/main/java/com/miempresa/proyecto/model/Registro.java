f08c56db99ac0980704fb0d29719c2f14e995550
Florencia Vergara <103700266+FlorenciaVergara@users.noreply.github.com>
2025-05-30 14:35:57 -0300

    Modelo de registro con cuit del responsable

---
+++ modelo-proyecto/src/main/java/com/miempresa/proyecto/model/Registro.java
@@ -0,0 +1,33 @@
+package com.miempresa.proyecto.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Registro {
+
+    @Id
+    private Integer codigo;
+    private String cuitResponsable;
+
+    public Registro() {}
+
+    public Registro(Integer codigo, String cuitResponsable) {
+        this.codigo = codigo;
+        this.cuitResponsable = cuitResponsable;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCuitResponsable() {
+        return cuitResponsable;
+    }
+    public void setCuitResponsable(String cuitResponsable) {
+        this.cuitResponsable = cuitResponsable;
+    }
+}
